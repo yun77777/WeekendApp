077a86943705c086229a7c229ee36e8a41d1116b
yun77777 <victoriousyk@naver.com>
2022-02-19 11:09:22 +0900

    WebView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn_move;
     private Button btn_shared;
+    private Button btn_wv;
     private EditText et_test;
     private String str;
     private ImageView test;
@@ -27,8 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
         btn_move = (Button)findViewById(R.id.btn_move);
         btn_shared = (Button)findViewById(R.id.btn_shared);
+        btn_wv = (Button)findViewById(R.id.btn_wv);
         et_test = (EditText)findViewById(R.id.et_test);
         test = (ImageView)findViewById(R.id.test);
+
+        btn_wv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra("extra","wv");
+                startActivity(intent);
+            }
+        });
 
         btn_shared.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/myapplication/WebViewActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebViewActivity extends AppCompatActivity {
+    private WebView webView;
+    private String url = "https://www.naver.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClientClass());
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
